11be43ab5e7ea0f9fc4ba4373786847b62568916
sangita <sbacchewar@dexciss.com>
2020-02-18 11:26:44 +0530

    in payslip dont have branch field

--- hr_branch_company/__manifest__.py
+++ hr_branch_company/__manifest__.py
@@ -3,7 +3,7 @@
     "name": "HR Branch",
     "summary": "An Branch is an organizational entity part of a "
                "company",
-    "version": "10.0.0.0.8",
+    "version": "10.0.0.0.9",
     "author": "Eficent, ",
     "website": "http://www.dexciss.com",
     "category": "HR Management",
@@ -12,7 +12,7 @@
     "data": [
         # "security/hr_security.xml",
         "view/hr_employee_view.xml",
-        # "view/hr_payslip_view.xml",
+        "view/hr_payslip_view.xml",
         "view/hr_leave_view.xml",
         # "view/hr_applicant_view.xml",
         # "view/hr_expence_view.xml",
