6e19c8e6bbea68a27bf981dd06e11c3167db3355
Rishab <gotorishab@gmail.com>
2020-03-02 13:47:58 +0530

    Leaves

---
+++ leave_holiday_report/models/recruitment_report.py
@@ -0,0 +1,40 @@
+from odoo import fields,api,models
+import math
+from datetime import datetime, timedelta, date
+from pytz import timezone, UTC
+from dateutil.relativedelta import relativedelta
+
+
+class LateComingReport(models.Model):
+    _name="resource.calendar.leaves.report"
+    _description="Resource Leave Report"
+
+
+    # employee_id = fields.Many2one('hr.employee',string="Employee")
+    branch_id = fields.Many2one('res.branch',string="Branch", store=True)
+    holiday_id = fields.Many2one('resource.calendar',string="Holiday Calendar")
+    name = fields.Char(string="Holiday")
+    date = fields.Date(string="Date")
+    month = fields.Char(string='Month')
+    is_current_month = fields.Boolean(compute="_check_current_month", store=True)
+    is_not_sat_sun = fields.Boolean(compute="_check_sat_sun", store=True)
+
+
+    @api.depends('date')
+    def _check_current_month(self):
+        for rec in self:
+            first_day = date.today().replace(day=1)
+            last_day = date.today().replace(day=1) + relativedelta(months=1) - relativedelta(days=1)
+            if rec.date:
+                if first_day <= rec.date <= last_day:
+                    rec.is_current_month = True
+                else:
+                    rec.is_current_month = False
+
+    @api.depends('month')
+    def _check_sat_sun(self):
+        for rec in self:
+            if rec.name == 'Sunday' or rec.name == 'Saturday':
+                rec.is_not_sat_sun = True
+            else:
+                rec.is_not_sat_sun = False
---
+++ leave_holiday_report/wizard/recruitment_view_wizard.py
@@ -0,0 +1,155 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+from datetime import timedelta, date, datetime
+import datetime
+from odoo.tools import float_utils
+from collections import defaultdict
+from pytz import utc
+
+
+class WizardLateComing(models.TransientModel):
+    _name = 'leave.holiday.wizard'
+    _description = 'Holiday Leave'
+    
+
+    # def _default_employee(self):
+    #     return self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)
+
+    def _default_from_date(self):
+        epoch_year = date.today().year
+        year_start = date(epoch_year, 1, 1)
+        return year_start
+
+    def _default_to_date(self):
+        epoch_year = date.today().year
+        year_end = date(epoch_year, 12, 31)
+        return year_end
+
+    @api.model
+    def _get_branch(self):
+        return self.env.user.default_branch_id
+    
+    report_of = fields.Selection([('leave','Leave Holiday'),
+                                  ],string="Report On", default='leave')
+    # employee_id = fields.Many2one('hr.employee','Requested By', default=_default_employee)
+    from_date = fields.Date(string='From Date', default=_default_from_date)
+    to_date = fields.Date(string='To Date', default=_default_to_date)
+    branch_ids = fields.Many2many('res.branch', string='Branches', default=_get_branch)
+
+
+    @api.multi
+    def confirm_report(self):
+        for rec in self:
+            dr = self.env['resource.calendar.leaves.report'].search([('branch_id', 'in', rec.branch_ids.ids)])
+            for lines in dr:
+                lines.unlink()
+            dr_b = self.env['resource.calendar.leaves'].search([('calendar_id.branch_id', 'in', rec.branch_ids.ids)])
+            for emp in dr_b:
+                print('========================================================',emp.date)
+            # for emp in rec.employee_id.resource_calendar_id.global_leave_ids:
+                if rec.from_date and emp.date and rec.to_date and rec.from_date <= emp.date <= rec.to_date:
+                    month = ''
+                    if str(emp.date.month) == '1':
+                        month = 'January'
+                    elif str(emp.date.month) == '2':
+                        month = 'February'
+                    elif str(emp.date.month) == '3':
+                        month = 'March'
+                    elif str(emp.date.month) == '4':
+                        month = 'April'
+                    elif str(emp.date.month) == '5':
+                        month = 'May'
+                    elif str(emp.date.month) == '6':
+                        month = 'June'
+                    elif str(emp.date.month) == '7':
+                        month = 'July'
+                    elif str(emp.date.month) == '8':
+                        month = 'August'
+                    elif str(emp.date.month) == '9':
+                        month = 'September'
+                    elif str(emp.date.month) == '10':
+                        month = 'October'
+                    elif str(emp.date.month) == '11':
+                        month = 'November'
+                    elif str(emp.date.month) == '12':
+                        month = 'December'
+                    else:
+                        month = ''
+                    cr = self.env['resource.calendar.leaves.report'].create({
+                        'name': emp.name,
+                        'date': emp.date,
+                        'branch_id': emp.calendar_id.branch_id.id,
+                        'holiday_id': emp.calendar_id.id,
+                        'month': month,
+                    })
+
+        return {
+            'name': 'Leave Holiday Report',
+            'view_type': 'form',
+            'view_mode': 'tree',
+            'res_model': 'resource.calendar.leaves.report',
+            'type': 'ir.actions.act_window',
+            'target': 'current',
+            'domain': [('branch_id', '=', self.branch_ids.ids)],
+            'context': {
+                            'name': 'Sunday',
+                        }
+            }
+
+
+    #
+    # @api.multi
+    # def confirm_existing_report(self):
+    #     for rec in self:
+    #         dr = self.env['resource.calendar.leaves.report'].search([('branch_id', 'in', rec.branch_ids.ids)])
+    #         for lines in dr:
+    #             lines.unlink()
+    #         emp_ids = self.env['hr.employee'].search([('branch_id', 'in', rec.branch_ids.ids)])
+    #         for employees in emp_ids:
+    #             for emp in employees.resource_calendar_id.global_leave_ids:
+    #                 if rec.from_date <= emp.date <= rec.to_date:
+    #                     month = ''
+    #                     if str(emp.date.month) == '1':
+    #                         month = 'January'
+    #                     elif str(emp.date.month) == '2':
+    #                         month = 'February'
+    #                     elif str(emp.date.month) == '3':
+    #                         month = 'March'
+    #                     elif str(emp.date.month) == '4':
+    #                         month = 'April'
+    #                     elif str(emp.date.month) == '5':
+    #                         month = 'May'
+    #                     elif str(emp.date.month) == '6':
+    #                         month = 'June'
+    #                     elif str(emp.date.month) == '7':
+    #                         month = 'July'
+    #                     elif str(emp.date.month) == '8':
+    #                         month = 'August'
+    #                     elif str(emp.date.month) == '9':
+    #                         month = 'September'
+    #                     elif str(emp.date.month) == '10':
+    #                         month = 'October'
+    #                     elif str(emp.date.month) == '11':
+    #                         month = 'November'
+    #                     elif str(emp.date.month) == '12':
+    #                         month = 'December'
+    #                     else:
+    #                         month = ''
+    #                     cr = self.env['resource.calendar.leaves.report'].create({
+    #                         'employee_id': rec.employee_id.id,
+    #                         'name': emp.name,
+    #                         'date': emp.date,
+    #                         'branch_id': emp.calendar_id.branch_id.id,
+    #                         'holiday_id': emp.calendar_id.id,
+    #                         'month': month,
+    #                     })
+    #
+    #     return {
+    #         'name': 'Leave Holiday Report',
+    #         'view_type': 'form',
+    #         'view_mode': 'tree',
+    #         'res_model': 'resource.calendar.leaves.report',
+    #         'type': 'ir.actions.act_window',
+    #         'target': 'current',
+    #         'domain': [('branch_id', 'in', self.branch_ids.ids)],
+    #     }
