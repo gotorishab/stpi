b3ded780d44b4703dc201b51fc047a9221e1a9e2
sangita <sbacchewar@dexciss.com>
2020-03-04 16:44:19 +0530

    dashbord ifram issue

--- dashboard_iframe/__manifest__.py
+++ dashboard_iframe/__manifest__.py
@@ -26,7 +26,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/dashboard_iframe.xml',
         'security/dashboard_iframe_access_rules.xml'
     ],
