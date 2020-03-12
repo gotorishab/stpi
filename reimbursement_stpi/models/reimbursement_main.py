c0f58355fd69a8af3188403dede00abcf32e2e92
Rishab <gotorishab@gmail.com>
2020-03-12 15:16:37 +0530

    TDS

--- reimbursement_stpi/models/reimbursement_main.py
+++ reimbursement_stpi/models/reimbursement_main.py
@@ -129,9 +129,9 @@ class Reimbursement(models.Model):
             search_id = self.env['reimbursement'].search([('employee_id', '=', rec.employee_id.id), ('name', '=', rec.name), ('state', 'not in', ['draft','rejected'])])
             index = False
             for emp in search_id:
-                if rec.date_range.date_start <= emp.from_date or rec.date_range.date_start >= emp.to_date:
-                    if rec.date_range.date_end <= emp.from_date or rec.date_range.date_end >= emp.to_date:
-                        if not (rec.date_range.date_start <= emp.from_date and rec.date_range.date_end >= emp.to_date):
+                if rec.date_range.date_start <= emp.date_range.date_start or rec.date_range.date_start >= emp.date_range.date_end:
+                    if rec.date_range.date_end <= emp.date_range.date_start or rec.date_range.date_end >= emp.date_range.date_end:
+                        if not (rec.date_range.date_start <= emp.date_range.date_start and rec.date_range.date_end >= emp.date_range.date_end):
                             index = True
                         else:
                             raise ValidationError("This reimbursement is already applied for this duration, please correct the dates")
