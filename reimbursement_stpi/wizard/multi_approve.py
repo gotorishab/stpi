5d664bc4f678173e0cad4a31fb08360f5c6e896d
Rishab <gotorishab@gmail.com>
2020-03-26 10:47:47 +0530

    STPI Changes

---
+++ reimbursement_stpi/wizard/multi_approve.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api, _
+from odoo.exceptions import UserError
+
+
+class ReimburementApprove(models.TransientModel):
+    _name = "reimbursement.approve"
+    _description = "Multiple Approve for reimbursements"
+
+
+    @api.multi
+    def reimbursement_approve_action_button(self):
+        context = dict(self._context or {})
+        active_ids = context.get('active_ids', []) or []
+        for rei in self.env['reimbursement'].browse(active_ids):
+            if rei.state == 'waiting_for_approval':
+                rei.sudo().button_approved()
+
+
+    @api.multi
+    def reimbursement_reject_action_button(self):
+        context = dict(self._context or {})
+        active_ids = context.get('active_ids', []) or []
+        for rei in self.env['reimbursement'].browse(active_ids):
+            if rei.state == 'waiting_for_approval':
+                rei.sudo().button_reject()
