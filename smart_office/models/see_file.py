1c4feb095c898d17cb7162c5ad188a7a2d23cce2
Rishab Gupta <gotorishab@gmail.com>
2020-06-01 10:40:17 +0530

    PF Report

--- smart_office/models/see_file.py
+++ smart_office/models/see_file.py
@@ -6,4 +6,10 @@ import json
 class FolderMaster(models.Model):
     _name = 'see.file'
 
-    my_url = fields.Html()
+    my_url = fields.Text()
+    url_attach = fields.Html()
+
+    @api.onchange('my_url')
+    def onchange_my_url(self):
+        if self.my_url:
+            self.url_attach = '<img id="img" src="%s"/>' % self.my_url
