a390ba6da18acb200853bf3294661f847f37c26b
Rishab Gupta <gotorishab@gmail.com>
2020-06-17 10:37:33 +0530

    Smart office

--- smart_office/wizard/write_correspondence.py
+++ smart_office/wizard/write_correspondence.py
@@ -43,6 +43,22 @@ class AddReference(models.TransientModel):
                 'state': 'draft',
             })
             dd.cooespondence_ids = [(6, 0, letter_id)]
+            form_view = self.env.ref('smart_office.foldermaster_form_view')
+            tree_view = self.env.ref('smart_office.foldermaster_tree_view1')
+            value = {
+                'domain': str([('id', '=', self.folder_id.id)]),
+                'view_type': 'form',
+                'view_mode': 'tree, form',
+                'res_model': 'folder.master',
+                'view_id': False,
+                'views': [(form_view and form_view.id or False, 'form'),
+                          (tree_view and tree_view.id or False, 'tree')],
+                'type': 'ir.actions.act_window',
+                'res_id': self.folder_id.id,
+                'target': 'current',
+                'nodestroy': True
+            }
+            return value
 
 
 
