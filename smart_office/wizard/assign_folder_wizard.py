1586fa5bbd88b2341847bd3255b2863800461ce0
Rishab Gupta <gotorishab@gmail.com>
2020-06-20 16:21:10 +0530

    Testing smart office

--- smart_office/wizard/assign_folder_wizard.py
+++ smart_office/wizard/assign_folder_wizard.py
@@ -21,20 +21,30 @@ class CreateFolder(models.TransientModel):
             for line in self.cooespondence_ids:
                 letter_id.append(line.id)
                 line.folder_id = self.folder_id.id
+                print('============line.php_letter_id=================', line.php_letter_id)
                 self.folder_id.document_ids = str(self.folder_id.document_ids) + ',' + str(line.php_letter_id)
             print('============self.folder_id.document_ids=================', self.folder_id.document_ids)
             self.folder_id.folder_ids = [(6, 0, letter_id)]
+            print('=======================assign_name========================', self.folder_id.folder_name)
+            print('=======================assign_no========================', self.folder_id.sequence)
+            print('=======================assign_date========================', self.folder_id.date)
+            print('=======================assign_subject========================', self.folder_id.subject.subject)
+            print('=======================rremarks========================', self.folder_id.description)
+            print('=======================created_by========================', self.folder_id.current_owner_id)
+            print('=======================wing_id========================', self.folder_id.department_id.id)
+            print('=======================designation_id========================', self.folder_id.job_id.id)
+            print('=======================document_ids========================', self.folder_id.document_ids)
             data = {
                 'assign_name': self.folder_id.folder_name,
                 'assign_no': self.folder_id.sequence,
                 'assign_date': self.folder_id.date,
                 'assign_subject': (self.folder_id.subject.subject),
                 'remarks': self.folder_id.description,
-                'created_by': 1,
+                'created_by': self.folder_id.current_owner_id.id,
                 'doc_flow_id': 0,
-                'wing_id': 1,
+                'wing_id': self.folder_id.department_id.id,
                 'section_id': 0,
-                'designation_id': 78,
+                'designation_id': self.folder_id.job_id.id,
                 'document_ids': self.folder_id.document_ids,
             }
             req = requests.post('http://103.92.47.152/STPI/www/web-service/add-assignment/', data=data,
@@ -45,8 +55,7 @@ class CreateFolder(models.TransientModel):
                 dictionary = json.loads(pastebin_url)
                 self.folder_id.iframe_dashboard = ''
                 self.folder_id.iframe_dashboard = str(dictionary["response"][0]['notesheet']) + str(
-                    '?type=STPI&user_id=') + str(
-                    self.env.user.id)
+                    '?type=STPI&user_id=') + str(self.folder_id.current_owner_id.id)
             except Exception as e:
                 print('=============Error==========', e)
             current_employee = self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)
