b095c0bd7eb5e8275d2bd1555a8dcd92ffe66e14
Rishab Gupta <rgupta@dexciss.com>
2020-07-27 13:51:49 +0530

    es

---
+++ employee_document_inherit/models/hr_employee.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api,_
+
+
+
+
+class EmployeeDocument(models.Model):
+    _inherit = 'hr.employee.document'
+
+
+    name = fields.Many2one('hr.employee.document.master', string='Document Name')
+
+    @api.onchange('name')
+    def get_desc_f_n(self):
+        for rec in self:
+            if rec.name.description:
+                rec.description = rec.name.description
+
+
+
+class DocMaster(models.Model):
+    _name = 'hr.employee.document.master'
+    _description = 'Employee Doc Master'
+
+    name = fields.Char('Name')
+    description = fields.Char('Description')
--- hr_employee_stpi/models/__init__.py
+++ hr_employee_stpi/models/__init__.py
@@ -6,6 +6,6 @@ from . import hr_employee
 from . import employee_previous_occupation
 from . import employee_training
 from . import hr_skills
-from . import hr_employee_document
+# from . import hr_employee_document
 from . import employee_stages_inherit
 from . import hr_leave_inherit
