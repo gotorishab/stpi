6abb5be836f47b239c55e49f1cd10a4b9ce107cf
Rishab Gupta <rgupta@dexciss.com>
2020-07-29 15:51:54 +0530

    Hr employee

--- hr_employee_directory_stpi/wizard/employee_action_select.py
+++ hr_employee_directory_stpi/wizard/employee_action_select.py
@@ -69,8 +69,8 @@ class EmployeeActionSelection(models.TransientModel):
                 'view_mode': 'tree,form',
                 'res_model': 'hr.employee',
                 'view_id': False,
-                'views': [(self.env.ref('hr_employee_stpi.view_add_employee_doc_tree').id, 'tree'),
-                          (self.env.ref('hr_employee_stpi.view_add_Employee_doc_form').id, 'form')],
+                'views': [(self.env.ref('hr_employee_directory_stpi.view_add_employee_doc_tree').id, 'tree'),
+                          (self.env.ref('hr_employee_directory_stpi.view_add_Employee_doc_form').id, 'form')],
                 'type': 'ir.actions.act_window'
             }
 
