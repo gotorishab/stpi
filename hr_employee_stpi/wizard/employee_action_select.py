716a1e40a77f66572d1955dfc00342be8778f0d8
Rishab Gupta <rgupta@dexciss.com>
2020-07-29 22:50:11 +0530

    Hr employee

--- hr_employee_stpi/wizard/employee_action_select.py
+++ hr_employee_stpi/wizard/employee_action_select.py
@@ -66,10 +66,11 @@ class EmployeeActionSelection(models.TransientModel):
                 'domain': [('id', 'in', my_ids)],
                 'name': 'Employees Directory(Branch)',
                 'view_type': 'form',
-                'view_mode': 'tree,form',
+                'view_mode': 'kanban,tree,form',
                 'res_model': 'hr.employee',
                 'view_id': False,
-                'views': [(self.env.ref('hr_employee_stpi.view_add_employee_doc_tree').id, 'tree'),
+                'views': [(self.env.ref('hr.hr_kanban_view_employees').id, 'kanban'),
+                          (self.env.ref('hr_employee_stpi.view_add_employee_doc_tree').id, 'tree'),
                           (self.env.ref('hr_employee_stpi.view_add_Employee_doc_form').id, 'form')],
                 'type': 'ir.actions.act_window'
             }
