ad30010b7fbae2f4e736f02b5f4d0eb33e10fb9a
Rishab Gupta <rgupta@dexciss.com>
2020-07-29 16:02:30 +0530

    Hr employee

--- hr_employee_stpi/__manifest__.py
+++ hr_employee_stpi/__manifest__.py
@@ -16,8 +16,8 @@
     'data': [
         'security/ir.model.access.csv',
         'security/hr_employee_security.xml',
-        # 'wizard/employee_action_select.xml',
-        # 'views/employee_b_view.xml',
+        'wizard/employee_action_select.xml',
+        'views/employee_b_view.xml',
         'views/hr_employee_main.xml',
         'views/employee_family_view.xml',
         'views/employee_previous_occupation.xml',
