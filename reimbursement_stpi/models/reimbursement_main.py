6b592f5aae9723f0970f1dcfc1e46ac2fa0c513a
Rishab Gupta <rgupta@dexciss.com>
2020-07-30 15:46:45 +0530

    Hr employee

--- reimbursement_stpi/models/reimbursement_main.py
+++ reimbursement_stpi/models/reimbursement_main.py
@@ -27,6 +27,9 @@ class Reimbursement(models.Model):
             rec.date_range = False
             if rec.name:
                 gr_id = self.env['reimbursement.configuration'].search(
+                    [('name', '=', rec.name), ('pay_level_ids', '=', rec.employee_id.job_id.pay_level_id.id), ('branch_id', '=', rec.branch_id.id),('job_ids', '=', rec.employee_id.job_id.id),('employee_type', '=', rec.employee_id.employee_type)], order='name desc', limit=1)
+                if not gr_id:
+                    gr_id = self.env['reimbursement.configuration'].search(
                     [('name', '=', rec.name), ('pay_level_ids', '=', rec.employee_id.job_id.pay_level_id.id), ('branch_id', '=', rec.branch_id.id)], order='name desc', limit=1)
                 return {'domain': {'date_range': [('type_id', '=', gr_id.date_range_type.id),('date_end', '<=', datetime.now().date())]}}
 
@@ -128,7 +131,9 @@ class Reimbursement(models.Model):
     @api.depends('claimed_amount')
     def compute_net_amount(self):
         for rec in self:
-            gr_id = self.env['reimbursement.configuration'].search([('name', '=', rec.name),('branch_id', '=', rec.branch_id.id),('pay_level_ids', '=', rec.employee_id.job_id.pay_level_id.id)],order='name desc', limit=1)
+            gr_id = self.env['reimbursement.configuration'].search([('name', '=', rec.name),('branch_id', '=', rec.branch_id.id),('pay_level_ids', '=', rec.employee_id.job_id.pay_level_id.id),('job_ids', '=', rec.employee_id.job_id.id),('employee_type', '=', rec.employee_id.employee_type)],order='name desc', limit=1)
+            if not gr_id:
+                gr_id = self.env['reimbursement.configuration'].search([('name', '=', rec.name),('branch_id', '=', rec.branch_id.id),('pay_level_ids', '=', rec.employee_id.job_id.pay_level_id.id)],order='name desc', limit=1)
             if gr_id:
                 if int(rec.claimed_amount) > int(gr_id.allowed) and gr_id.full == False:
                     rec.net_amount = gr_id.allowed
@@ -174,6 +179,11 @@ class Reimbursement(models.Model):
                         "Amount must be greater than zero")
                 else:
                     gr_id = self.env['reimbursement.configuration'].search(
+                        [('name', '=', rec.name),('branch_id', '=', rec.branch_id.id), ('pay_level_ids', '=', rec.employee_id.job_id.pay_level_id.id),('job_ids', '=', rec.employee_id.job_id.id),('employee_type', '=', rec.employee_id.employee_type)],
+                        order='name desc',
+                        limit=1)
+                    if not gr_id:
+                        gr_id = self.env['reimbursement.configuration'].search(
                         [('name', '=', rec.name),('branch_id', '=', rec.branch_id.id), ('pay_level_ids', '=', rec.employee_id.job_id.pay_level_id.id)],
                         order='name desc',
                         limit=1)
