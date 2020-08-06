457b35ac7525ef69f9eba75176a7009e456011c1
Rishab Gupta <rgupta@dexciss.com>
2020-08-06 15:42:07 +0530

    STPI changes

--- pf_withdrawl/__manifest__.py
+++ pf_withdrawl/__manifest__.py
@@ -19,6 +19,7 @@
         'views/salary_rule.xml',
         'views/pf_type_view.xml',
         'views/pf_configuration.xml',
+        'views/res_companies.xml',
 	],
 	'images': [],
 	'license': 'AGPL-3',
--- pf_withdrawl/models/pf_interest_disbursement.py
+++ pf_withdrawl/models/pf_interest_disbursement.py
@@ -9,7 +9,20 @@ class PfInterestDisbursement(models.Model):
     branch_id = fields.Many2many('res.branch')
     from_date = fields.Date('From Date')
     to_date = fields.Date('To Date')
-    interest_rate = fields.Float('Interest Rate')\
+    interest_rate = fields.Float('Interest Rate')
+
+
+    @api.constrains('from_date','to_date','branch_id')
+    @api.onchange('from_date','to_date','branch_id')
+    def onchange_date_branch_gi(self):
+        for rec in self:
+            company = self.env['res.company'].search([('id', '=', self.env.user.company_id.id)], limit=1)
+            if company:
+                for com in company:
+                    if rec.from_date and rec.to_date and rec.branch_id:
+                        for line in com.pf_table:
+                            if line.from_date >= rec.from_date and line.to_date <= rec.to_date:
+                                rec.interest_rate = line.interest_rate
 
 
     @api.multi
