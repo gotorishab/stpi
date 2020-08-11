41616248e580e91066160074a5f749037b8f7702
Rishab Gupta <rgupta@dexciss.com>
2020-08-11 15:55:32 +0530

    PF

--- smart_office/wizard/filewizard.py
+++ smart_office/wizard/filewizard.py
@@ -86,9 +86,9 @@ class FileWizard(models.Model):
                     'details': 'Correspondence Forwarded'
                 })
             else:
-                self.defid.current_owner_id = self.env.user.id
-                self.confirm_button()
-                # raise ValidationError("You are not able to forward this file, as you are not the Primary owner of this file")
+                # self.defid.current_owner_id = self.env.user.id
+                # self.confirm_button()
+                raise ValidationError("You are not able to forward this file, as you are not the Primary owner of this file")
         return {
             'name': 'Incoming correspondence',
             'view_type': 'form',
