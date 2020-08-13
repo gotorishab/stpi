50762567fad7a04634058fbf987df83ef6c47b6d
Rishab Gupta <rgupta@dexciss.com>
2020-08-13 16:06:47 +0530

    eFile

--- stpi_contract_pr/models/res_city_inherit.py
+++ stpi_contract_pr/models/res_city_inherit.py
@@ -9,7 +9,7 @@ class InheritResCity(models.Model):
     city_tier = fields.Selection([('a', 'A'),
                                   ('a1', 'A1'),
                                   ('other', 'Other'),
-                                  ], string='City Tier', compute='_compute_hra_tier', store=True)
+                                  ], string='City Tier', store=True)
     employee_hra_cat = fields.Selection([('x', 'X'),
                                          ('y', 'Y'),
                                          ('z', 'Z'),
