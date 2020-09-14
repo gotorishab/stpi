9191784614873de8f5519a28f142f1e046692cb2
Rishab Gupta <rgupta@dexciss.com>
2020-09-14 11:29:06 +0530

    STPI changes

--- employee_ltc/models/employee_ltc.py
+++ employee_ltc/models/employee_ltc.py
@@ -403,6 +403,8 @@ class EmployeeLtcAdvance(models.Model):
                 count_home = 0
                 for ltc_pre in val_ids:
                     if ltc_pre.ltc_id != res.id:
+                        print('===============ledger id 1========================', ltc_pre.ltc_id)
+                        print('===============My id 1========================', res.id)
                         if ltc_pre.place_of_trvel == res.place_of_trvel and ltc_pre.block_year == res.block_year and ltc_pre.child_block_year == res.child_block_year:
                                 raise ValidationError(
                                     _('You are not allowed to take LTC for this block year'))
@@ -426,6 +428,8 @@ class EmployeeLtcAdvance(models.Model):
                 count_india = 0
                 for ltc_pre in val_ids:
                     if ltc_pre.ltc_id != res.id:
+                        print('===============ledger id 2========================', ltc_pre.ltc_id)
+                        print('===============My id 2========================', res.id)
                         if ltc_pre.place_of_trvel == res.place_of_trvel and ltc_pre.block_year == res.block_year and ltc_pre.child_block_year == res.child_block_year:
                                 raise ValidationError(
                                     _('You are not allowed to take LTC for this block year'))
@@ -452,6 +456,8 @@ class EmployeeLtcAdvance(models.Model):
                 count_home = 0
                 for ltc_pre in rel_ids:
                     if ltc_pre.ltc_id != res.id:
+                        print('===============ledger id 3========================', ltc_pre.ltc_id)
+                        print('===============My id 3========================', res.id)
                         if ltc_pre.place_of_trvel == res.place_of_trvel and ltc_pre.block_year == res.block_year and ltc_pre.child_block_year == res.child_block_year:
                                 raise ValidationError(
                                     _('You are not allowed to take LTC for this block year'))
@@ -475,6 +481,8 @@ class EmployeeLtcAdvance(models.Model):
                 count_india = 0
                 for ltc_pre in rel_ids:
                     if ltc_pre.ltc_id != res.id:
+                        print('===============ledger id 4========================', ltc_pre.ltc_id)
+                        print('===============My id 4========================', res.id)
                         if res.place_of_trvel == ltc_pre.place_of_trvel and res.block_year == ltc_pre.block_year and res.child_block_year == ltc_pre.child_block_year:
                             raise ValidationError(
                                 _('You are not allowed to take LTC for this block year'))
