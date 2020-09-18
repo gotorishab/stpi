96357e3b9a96b4d9235acff1b79b2405bb0a373f
Rishab Gupta <rgupta@dexciss.com>
2020-09-18 11:20:05 +0530

    STPI changes

--- stpi_payroll inherit/wizard/hr_payment_advices_by_employees_views.py
+++ stpi_payroll inherit/wizard/hr_payment_advices_by_employees_views.py
@@ -10,31 +10,31 @@ class HrPayslipEmployees(models.TransientModel):
     _description = 'Generate Payment Advices for all selected employees'
 
     employee_ids = fields.Many2many('hr.employee', string='Employees')
+    advice = fields.Many2many('hr.payroll.advice', string='Advice ID')
 
     @api.multi
     def compute_sheet(self):
-        pass
-        # """
-        #         Advice - Create Advice lines in Payment Advice and
-        #         compute Advice lines.
-        #         """
+        """
+                Advice - Create Advice lines in Payment Advice and
+                compute Advice lines.
+                """
         # for advice in self:
-        #     old_lines = self.env['hr.payroll.advice.line'].search([('advice_id', '=', advice.id)])
-        #     if old_lines:
-        #         old_lines.unlink()
-        #     payslips = self.env['hr.payslip'].search(
-        #         [('date_from', '<=', advice.date), ('date_to', '>=', advice.date), ('state', '=', 'done')])
-        #     for slip in payslips:
-        #         if not slip.employee_id.bank_account_number:
-        #             raise UserError(_('Please define bank account for the %s employee') % (slip.employee_id.name,))
-        #         payslip_line = self.env['hr.payslip.line'].search([('slip_id', '=', slip.id), ('code', '=', 'NET')],
-        #                                                           limit=1)
-        #         if payslip_line:
-        #             self.env['hr.payroll.advice.line'].create({
-        #                 'advice_id': advice.id,
-        #                 'name': slip.employee_id.bank_account_number,
-        #                 'ifsc_code': slip.employee_id.ifsc_code or '',
-        #                 'employee_id': slip.employee_id.id,
-        #                 'bysal': payslip_line.total
-        #             })
-        #         slip.advice_id = advice.id
+        old_lines = self.env['hr.payroll.advice.line'].search([('advice_id', '=', self.advice.id)])
+        if old_lines:
+            old_lines.unlink()
+        payslips = self.env['hr.payslip'].search(
+            [('date_from', '<=', self.advice.date), ('date_to', '>=', self.advice.date), ('state', '=', 'done'), ('employee_id', 'in', self.employee_ids.ids)])
+        for slip in payslips:
+            if not slip.employee_id.bank_account_number:
+                raise UserError(_('Please define bank account for the %s employee') % (slip.employee_id.name,))
+            payslip_line = self.env['hr.payslip.line'].search([('slip_id', '=', slip.id), ('code', '=', 'NET')],
+                                                              limit=1)
+            if payslip_line:
+                self.env['hr.payroll.advice.line'].create({
+                    'advice_id': self.advice.id,
+                    'name': slip.employee_id.bank_account_number,
+                    'ifsc_code': slip.employee_id.ifsc_code or '',
+                    'employee_id': slip.employee_id.id,
+                    'bysal': payslip_line.total
+                })
+            slip.advice_id = self.advice.id
