c15161d0c8831a0dfe885789fe2a6e983f05f09f
Rishab Gupta <rgupta@dexciss.com>
2020-09-19 14:04:09 +0530

    STPI changes

--- groups_inherit/wizard/create_user_wizard.py
+++ groups_inherit/wizard/create_user_wizard.py
@@ -1,6 +1,4 @@
 from odoo import api, fields, models,_
-import requests
-import json
 
 
 class createuser_wizard(models.TransientModel):
@@ -18,7 +16,6 @@ class createuser_wizard(models.TransientModel):
     groups_id = fields.Many2many('res.groups',string='Groups', domain="[('stpi', '=', True)]")
     branch_ids = fields.Many2many('res.branch', default=_get_branch)
     default_branch_id = fields.Many2one('res.branch', string='Default branch',default=_get_branch)
-    employee_id = fields.Many2one('hr.employee', string='Employee')
 
     def button_confirm(self):
         for rec in self:
@@ -35,36 +32,12 @@ class createuser_wizard(models.TransientModel):
             })
             for group in rec.groups_id:
                 group.users += user
-            data = {
-                'name': rec.name,
-                'username': rec.login,
-                'password': rec.password,
-                'userid': user.id,
-                'wing_id': rec.employee_id.department_id.id,
-                'section_id': 1,
-                'designation_id': rec.employee_id.job_id.id,
-                'email': rec.login,
-                'mobile': rec.employee_id.phone,
-                'user_type_id': 3,
-                'is_wing_head': rec.name,
-                'user_id': rec.env.uid,
-            }
-
-            req = requests.post('http://103.92.47.152/STPI/www/web-service/add-user/', data=data,
-                                json=None)
-            try:
-                # print('=====================================================', req)
-                pastebin_url = req.text
-                print('===========================pastebin_url==========================', pastebin_url)
-                dictionary = json.loads(pastebin_url)
-                comp_model = self.env['res.users'].search([('login', '=', rec.login)], limit=1)
-                _body = (_(
-                    (
-                        "<ul><b>User Created: {0} </b></ul> ").format(rec.login)))
-                model_id.message_post(body=_body)
-                model_id.user_id = comp_model.id
-            except Exception as e:
-                print('=============Error==========', e)
+            comp_model = self.env['res.users'].search([('login', '=', rec.login)], limit=1)
+            _body = (_(
+                (
+                    "<ul><b>User Created: {0} </b></ul> ").format(rec.login)))
+            model_id.message_post(body=_body)
+            model_id.user_id = comp_model.id
 
 
 
