c2690e099cf1cb9705c3c4da8488cb0517c64d46
Rishab Gupta <rgupta@dexciss.com>
2020-09-19 13:58:28 +0530

    STPI changes

--- smart_office/models/res_users.py
+++ smart_office/models/res_users.py
@@ -11,23 +11,73 @@ class ResUesrs(models.Model):
     def create(self, vals):
         res = super(ResUesrs, self).create(vals)
         data = {
-            'name': res.document_type,
-            'username': int(seq),
-            'password': enclosure_details,
-            'user_id': current_employee.user_id.id,
-            'attachment[]': res.content
+            'name': res.name,
+            'username': res.login,
+            'password': res.password,
+            'userid': res.name,
+            'wing_id': res.name,
+            'section_id': res.name,
+            'designation_id': res.name,
+            'email': res.login,
+            'mobile': res.name,
+            'user_type_id': res.name,
+            'is_wing_head': res.name,
+            'user_id': res.name,
         }
-        print('==============================name=============================', int(seq))
-        print('==============================enclosure_details=============================', enclosure_details)
-        print('==============================user_id=============================', current_employee.user_id.id)
-        # print('==============================res.content=============================', res.content)
-        req = requests.post('http://103.92.47.152/STPI/www/web-service/add-letter/', data=data,
+
+        req = requests.post('http://103.92.47.152/STPI/www/web-service/add-user/', data=data,
                             json=None)
         try:
-            print('=====================================================', req)
+            # print('=====================================================', req)
             pastebin_url = req.text
             print('===========================pastebin_url==========================', pastebin_url)
             dictionary = json.loads(pastebin_url)
-            res.php_letter_id = str(dictionary["response"]["letterData"]["id"])
+        except Exception as e:
+            print('=============Error==========', e)
+
+
+
+class HrDepartment(models.Model):
+    _inherit= 'hr.department'
+
+    @api.model
+    def create(self, vals):
+        res = super(HrDepartment, self).create(vals)
+        data = {
+            'name': res.name,
+            'dept_id': res.name,
+            'user_id': res.name,
+        }
+
+        req = requests.post('http://103.92.47.152/STPI/www/web-service/department/', data=data,
+                            json=None)
+        try:
+            # print('=====================================================', req)
+            pastebin_url = req.text
+            print('===========================pastebin_url==========================', pastebin_url)
+            dictionary = json.loads(pastebin_url)
+        except Exception as e:
+            print('=============Error==========', e)
+
+
+class HrJob(models.Model):
+    _inherit= 'hr.job'
+
+    @api.model
+    def create(self, vals):
+        res = super(HrJob, self).create(vals)
+        data = {
+            'name': res.name,
+            'designation_id': res.name,
+            'user_id': res.name,
+        }
+
+        req = requests.post('http://103.92.47.152/STPI/www/web-service/designation/', data=data,
+                            json=None)
+        try:
+            # print('=====================================================', req)
+            pastebin_url = req.text
+            print('===========================pastebin_url==========================', pastebin_url)
+            dictionary = json.loads(pastebin_url)
         except Exception as e:
             print('=============Error==========', e)
