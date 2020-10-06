c33b07b99d0fc79555aa7b9df2ecc20462cb3ecf
Rishab Gupta <rgupta@dexciss.com>
2020-10-06 14:04:05 +0530

    API

--- hr_employee_stpi/controllers/employee_user.py
+++ hr_employee_stpi/controllers/employee_user.py
@@ -9,7 +9,7 @@ class FileForwardData(http.Controller):
     @http.route(['/create_user'], type='http', auth='public', csrf=False, methods=['POST'])
     def create_hrmis_user(self, name=None, login=None, email=None, password=None, **kwargs):
         user_det = []
-        if login and name and email:
+        if login and name and email and password:
             user_details_data = request.env['res.users'].sudo().create({
                 'name': name,
                 'login': login,
@@ -35,6 +35,43 @@ class FileForwardData(http.Controller):
                 loaded_r = json.dumps(dict(response=str(message)))
                 return loaded_r
         else:
-            message = "Please pass login, email and name"
+            message = "Please pass login, email and name and password"
             loaded_r = json.dumps(dict(response=str(message)))
             return loaded_r
+
+
+
+    @http.route(['/create_users'], type='json', auth='none',  methods=['POST'])
+    def create_hrmis_users(self, name=None, login=None, email=None, password=None, **kwargs):
+        if request.jsonrequest:
+            user_det = []
+            if login and name and email and password:
+                user_details_data = request.env['res.users'].sudo().create({
+                    'name': name,
+                    'login': login,
+                    'password': '1234',
+                    'confirm_password': '1234',
+                    'email': email,
+                    'notification_type': 'inbox',
+                    'sel_groups_1_9_10':1,
+                        })
+                if user_details_data:
+                    for rec in user_details_data:
+                        vals = {
+                            'name': name,
+                            'login': login,
+                            'email': email,
+                            'password': password,
+                        }
+                        user_det.append(vals)
+                    loaded_r = json.dumps(dict(response=str(user_det)))
+                    return loaded_r
+                else:
+                    message = "User not created"
+                    loaded_r = json.dumps(dict(response=str(message)))
+                    return loaded_r
+            else:
+                message = "Please pass login, email and name and password"
+                loaded_r = json.dumps(dict(response=str(message)))
+                return loaded_r
+
