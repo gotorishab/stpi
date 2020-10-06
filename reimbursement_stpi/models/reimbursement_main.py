bc41752875dd1f10b9d146e8613647c9e420c915
Rishab Gupta <rgupta@dexciss.com>
2020-10-06 09:06:02 +0530

    Reimbursement

--- reimbursement_stpi/models/reimbursement_main.py
+++ reimbursement_stpi/models/reimbursement_main.py
@@ -130,6 +130,7 @@ class Reimbursement(models.Model):
         active_ids = context.get('active_ids', []) or []
         for employee in self.env['reimbursement'].browse(active_ids):
             lst.append(employee.id)
+        print('===========lst===============',lst)
         return self.env['reimbursement'].search([('id', 'in', lst)])
 
     @api.constrains('working_days')
