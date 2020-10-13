c62658921036c31e19ada357bbae068e0a38e6ee
Rishab Gupta <rgupta@dexciss.com>
2020-10-13 11:58:18 +0530

    HR Applicant

--- recruitment_up_stpi/models/job_opening.py
+++ recruitment_up_stpi/models/job_opening.py
@@ -40,59 +40,70 @@ class RecruitmentJobOpening(models.Model):
             rec.write({'state': 'rejected'})
 
 
+    @api.multi
+    def button_create_advertisemtnt(self):
+        for rec in self:
+            create_advertisement = self.env['hr.requisition.application'].create(
+                {
+                    'state': 'draft',
+                    'branch_id': rec.branch_id.id,
+                    'start_date': datetime.now().date(),
+                }
+            )
+            for line in rec.job_pos:
+                create_advertisement_line = self.env['advertisement.line'].create(
+                    {
+                        'allowed_category_id': create_advertisement.id,
+                        'job_id': line.job_id.id,
+                        'branch_id': line.branch.id,
+                        'opening': int(line.roster_line_id.sc) + int(line.roster_line_id.general) + int(line.roster_line_id.st),
+                        'sc': int(line.roster_line_id.sc),
+                        'general': int(line.roster_line_id.general),
+                        'st': int(line.roster_line_id.st),
+                    }
+                )
+
+            rec.write({'state': 'published'})
+
+
     # @api.multi
     # def button_create_advertisemtnt(self):
     #     for rec in self:
+    #         lst1 = []
+    #         combcount = 1
+    #         for line in rec.job_pos:
+    #             comb = str(line.job_id) + ' ' + str(line.branch_id)
+    #             lst1.append(comb)
+    #
+    #         word_dict = {}
+    #         for words in lst1:
+    #             if words in word_dict.keys():
+    #                 word_dict[words] += 1
+    #             else:
+    #                 word_dict[words] = 1
+    #         print('==========================', word_dict)
+    #
+    #
+    #         # detail_of_journey = []
     #         create_advertisement = self.env['hr.requisition.application'].create(
     #             {
     #                 'state': 'draft',
     #                 'branch_id': rec.branch_id.id,
     #                 'start_date': datetime.now().date(),
+    #                 # 'advertisement_line_ids':
+    #                 #     ((0, 0, {
+    #                 #     'job_ids': rec.id,
+    #                 #     'branch_id': i.departure_date,
+    #                 #     'department_id': i.departure_time,
+    #                 #     'opening': i.arrival_date,
+    #                 #     'sc': i.arrival_time,
+    #                 #     'general': i.from_l.id,
+    #                 #     'st': i.to_l.id,
+    #                 #     }))
     #             }
     #         )
     #         rec.write({'state': 'published'})
-
-
-    @api.multi
-    def button_create_advertisemtnt(self):
-        for rec in self:
-            lst1 = []
-            combcount = 1
-            for line in rec.job_pos:
-                comb = str(line.job_id) + ' ' + str(line.branch_id)
-                lst1.append(comb)
-
-            word_dict = {}
-            for words in lst1:
-                if words in word_dict.keys():
-                    word_dict[words] += 1
-                else:
-                    word_dict[words] = 1
-            print('==========================', word_dict)
-
-
-
-            #
-            # # detail_of_journey = []
-            # create_advertisement = self.env['hr.requisition.application'].create(
-            #     {
-            #         'state': 'draft',
-            #         'branch_id': rec.branch_id.id,
-            #         'start_date': datetime.now().date(),
-            #         # 'advertisement_line_ids':
-            #         #     ((0, 0, {
-            #         #     'job_ids': rec.id,
-            #         #     'branch_id': i.departure_date,
-            #         #     'department_id': i.departure_time,
-            #         #     'opening': i.arrival_date,
-            #         #     'sc': i.arrival_time,
-            #         #     'general': i.from_l.id,
-            #         #     'st': i.to_l.id,
-            #         #     }))
-            #     }
-            # )
-            # rec.write({'state': 'published'})
-            # print('==================================================', create_advertisement.id)
+    #         print('==================================================', create_advertisement.id)
 
 
 class RecruitmentJobLines(models.Model):
