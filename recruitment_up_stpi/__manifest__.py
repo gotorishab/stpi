502ee04a8af4a25e0f1fcc376ff55113c276c54e
Rishab Gupta <rgupta@dexciss.com>
2020-10-14 11:50:31 +0530

    STPI

--- recruitment_up_stpi/__manifest__.py
+++ recruitment_up_stpi/__manifest__.py
@@ -8,7 +8,7 @@
 	'maintainer': 'dexciss Techno Solutions',
 	'company': 'dexciss Techno Solutions',
 	'website': 'https://www.dexciss.com',
-	'depends': ['base','mail'],
+	'depends': ['base','mail','hr_employee_stpi'],
 	'data': [
 		'security/ir.model.access.csv',
 		'security/security.xml',
