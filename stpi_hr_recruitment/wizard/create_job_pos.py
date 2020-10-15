899280de44a6dcc6580b6cce05f5cdfeba5020c7
Rishab Gupta <rgupta@dexciss.com>
2020-10-15 11:03:47 +0530

    Job position

--- stpi_hr_recruitment/wizard/create_job_pos.py
+++ stpi_hr_recruitment/wizard/create_job_pos.py
@@ -15,17 +15,17 @@ class CreateJobPos(models.TransientModel):
 
     def confirm_job_pos(self):
         if self:
-            pass
-            # advertisement_line_ids = []
-            # for line in self.job_recruit:
-            #     for inline in line.job_pos:
-            #         advertisement_line_ids.append((0, 0, {
-            #             'allowed_category_id': self.advertisement_id.id,
-            #             'job_id': inline.job_id.id,
-            #             'branch_id': inline.branch_id.id,
-            #             'category_id': inline.category_id.id,
-            #             'state': inline.category_id.id,
-            #             'remarks': inline.remarks,
-            #             'opening': 1,
-            #         }))
-            #         self.advertisement_id.advertisement_line_ids = advertisement_line_ids
+            # pass
+            advertisement_line_ids = []
+            for line in self.job_recruit:
+                for inline in line.job_pos:
+                    advertisement_line_ids.append((0, 0, {
+                        'allowed_category_id': self.advertisement_id.id,
+                        'job_id': inline.job_id.id,
+                        'branch_id': inline.branch_id.id,
+                        'category_id': inline.category_id.id,
+                        'state': inline.category_id.id,
+                        'remarks': inline.remarks,
+                        'opening': 1,
+                    }))
+                    self.advertisement_id.advertisement_line_ids = advertisement_line_ids
