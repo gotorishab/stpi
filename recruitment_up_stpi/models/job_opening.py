4728cb3cde30c6b7f1c2a745901779159d0655c6
Rishab Gupta <rgupta@dexciss.com>
2020-10-16 12:16:05 +0530

    Job position

--- recruitment_up_stpi/models/job_opening.py
+++ recruitment_up_stpi/models/job_opening.py
@@ -7,17 +7,29 @@ class RecruitmentJobOpening(models.Model):
     _name = "recruitment.jobop"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Job Opening"
+    _rec_name = 'name'
 
     def default_employee(self):
         return self.env['hr.employee'].search([('user_id', '=', self.env.user.id)], limit=1)
 
+    def default_branch(self):
+        return self.env.user.default_branch_id
 
+    name = fields.Char('Name')
     requested_by = fields.Many2one('hr.employee', string='Requested By', default=default_employee)
     requested_on = fields.Date(string="Requested On", default=fields.Date.today(),track_visibility='always')
-    branch_id = fields.Many2one('res.branch', string='Branch')
+    branch_id = fields.Many2one('res.branch', string='Branch', default=default_branch)
     job_pos = fields.One2many('job.opening.lines', 'job_opening_id', string='Job Openings')
     state = fields.Selection([('draft', 'Draft'), ('to_approve', 'To Approve'), ('approved', 'Approved'), ('published', 'Published'), ('rejected', 'Rejected')], required=True, string='Status', default='draft', track_visibility='always')
 
+    @api.model
+    def create(self, vals):
+        res = super(RecruitmentJobOpening, self).create(vals)
+        sequence = ''
+        seq = self.env['ir.sequence'].next_by_code('recruitment.jobop')
+        sequence = 'Job Op - ' + str(seq)
+        res.name = sequence
+        return res
 
     @api.multi
     def button_draft(self):
