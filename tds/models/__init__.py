606596473dd1e52d125dfd51bcf1e01d90c68cac
Rishab Gupta <rgupta@dexciss.com>
2020-10-20 11:39:04 +0530

    TDS

--- tds/models/__init__.py
+++ tds/models/__init__.py
@@ -1,8 +1,8 @@
 from . import hr_payslip
 from . import tds
 from . import hr_declaration
-# from . import hr_payroll
+from . import hr_payroll
 from . import date_range
 from . import hr_declaration_cron
 from . import non_documents_report
-from . import n_hr_payroll
+# from . import n_hr_payroll
