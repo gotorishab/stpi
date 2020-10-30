1ad616a32ae934c05d5a9a1dcf95d95aa5b96e6f
Rishab Gupta <rgupta@dexciss.com>
2020-10-30 13:45:43 +0530

    eFile

--- smart_office/wizard/part_file.py
+++ smart_office/wizard/part_file.py
@@ -17,19 +17,3 @@ class CreateFolder(models.TransientModel):
         if self:
             for file in self.folder_id:
                 file.sudo().button_close()
-            form_view = self.env.ref('smart_office.foldermaster_form_view')
-            tree_view = self.env.ref('smart_office.foldermaster_tree_view1')
-            value = {
-                'domain': str([('id', '=', self.folder_id.id)]),
-                'view_type': 'form',
-                'view_mode': 'tree, form',
-                'res_model': 'folder.master',
-                'view_id': False,
-                'views': [(form_view and form_view.id or False, 'form'),
-                          (tree_view and tree_view.id or False, 'tree')],
-                'type': 'ir.actions.act_window',
-                'res_id': self.folder_id.id,
-                'target': 'current',
-                'nodestroy': True
-            }
-            return value
