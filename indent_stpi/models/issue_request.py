5dfb01dc5696ede17b6321b6361c00b058fcbddc
Rishab Gupta <rgupta@dexciss.com>
2020-12-17 09:34:41 +0530

    Indent

--- indent_stpi/models/issue_request.py
+++ indent_stpi/models/issue_request.py
@@ -95,7 +95,6 @@ class IndentLedger(models.Model):
         for rec in self:
             rec.write({'state': 'rejected'})
 
-
     @api.multi
     def fill_asset_details(self):
         server_connection_id = self.env['server.connection'].search([('active', '=', True)])
@@ -103,11 +102,14 @@ class IndentLedger(models.Model):
         db = server_connection_id.db_name
         common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
         models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
-        print('==========models================', models)
-        uid = self.env.user.id
-        password = self.env.user.password
-        id = models.execute_kw(db, 2, "admin", 'account.asset.asset', 'create', [{"name": self.item_id.name,"serial_number": self.serial_number,"category_id":1,"value":1}])
-        print('==========asset_data================', id)
+        # print('==========models================', models)
+        uid = 2
+        # password = self.env.user.password
+        password = 'admin'
+        id = models.execute_kw(db, uid, password, 'account.asset.asset', 'create',
+                               [{"name": self.item_id.name, "serial_number": self.serial_number,
+                                 "category_id": 1, "value": 1, 'login': self.env.user.login}])
+        # print('==========asset_data================', id)
         self.coe_asset_id = id
         asset_id = id
         key = ",jy`\;4Xpe7%KKL$.VNJ'.s6)wErQa"
