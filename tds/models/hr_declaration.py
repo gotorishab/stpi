4e8db794e3f1bb78f7631c6859f9eca21c6723a6
Rishab Gupta <rgupta@dexciss.com>
2021-02-17 19:00:57 +0530

    TDS

--- tds/models/hr_declaration.py
+++ tds/models/hr_declaration.py
@@ -929,25 +929,33 @@ class HrDeclaration(models.Model):
                 if income_slab:
                     remaining_amt = rec.taxable_income
                     for inc in income_slab:
-
-                        if remaining_amt > 0 and rec.taxable_income >= inc.salary_to and remaining_amt <= inc.salary_from:
-                            tax_amt = (((inc.salary_to - inc.salary_from) * inc.tax_rate) / 100)
+                        if remaining_amt < inc.salary_to:
+                            tax_amt = (remaining_amt * inc.tax_rate) / 100
                             total_tax_amt += tax_amt
-                        elif remaining_amt > 0:
-                            if remaining_amt >= inc.salary_from:
-                                tax_amt = ((remaining_amt * inc.tax_rate) / 100)
+                        else:
+                            tax_amt = ((inc.salary_to - inc.salary_from) * inc.tax_rate) /100
                             total_tax_amt += tax_amt
+                        remaining_amt = remaining_amt - (inc.salary_to - inc.salary_from)
 
-                        remaining_amt = (remaining_amt - (inc.salary_to - inc.salary_from))
+                        #
+                        # if remaining_amt > 0 and rec.taxable_income >= inc.salary_to and remaining_amt <= inc.salary_from:
+                        #     tax_amt = (((inc.salary_to - inc.salary_from) * inc.tax_rate) / 100)
+                        #     total_tax_amt += tax_amt
+                        # elif remaining_amt > 0:
+                        #     if remaining_amt >= inc.salary_from:
+                        #         tax_amt = ((remaining_amt * inc.tax_rate) / 100)
+                        #     total_tax_amt += tax_amt
+                        #
+                        # remaining_amt = (remaining_amt - (inc.salary_to - inc.salary_from))
                         _body = (_(
                             ("salary to: {0} - salary from: {1} - total tax amount: {2} - remaining amt: {3}").format(
                                 inc.salary_to, inc.salary_from, total_tax_amt,
                                 remaining_amt)))
-                        rec.message_post(body=_body)
-
-                        surcharge = inc.surcharge
-                        cess = inc.cess
-
+                        # rec.message_post(body=_body)
+                        #
+                        # surcharge = inc.surcharge
+                        # cess = inc.cess
+                        #
                         if remaining_amt <= 0:
                             break
                     n_sur = (total_tax_amt*surcharge/100)
