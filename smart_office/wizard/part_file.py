69f0769904e1988c2c98969beec79ab5a1c2e9d3
Rishab Gupta <rgupta@dexciss.com>
2021-03-04 17:15:17 +0530

    Appraisal

--- smart_office/wizard/part_file.py
+++ smart_office/wizard/part_file.py
@@ -20,6 +20,27 @@ class CreateFolder(models.TransientModel):
                     letter.folder_id = self.deffolderid.id
                     self.deffolderid.file_ids = [(4, letter.id)]
                     self.deffolderid.document_ids = str(self.deffolderid.document_ids) + ',' + str(letter.php_letter_id)
+                    data = {
+                        'assign_name': self.folder_id.folder_name,
+                        'assign_no': self.folder_id.sequence,
+                        'assign_date': self.folder_id.date,
+                        'assign_subject': (self.folder_id.subject.subject),
+                        'remarks': self.folder_id.description,
+                        'created_by': self.folder_id.current_owner_id.id,
+                        'doc_flow_id': 0,
+                        'wing_id': self.folder_id.department_id.id,
+                        'section_id': 0,
+                        'designation_id': self.folder_id.job_id.id,
+                        'document_ids': self.folder_id.document_ids,
+                    }
+                    req = requests.post('http://103.92.47.152/STPI/www/web-service/add-assignment/', data=data,
+                                        json=None)
+                    try:
+                        pastebin_url = req.text
+                        print('============Patebin url=================', pastebin_url)
+                        dictionary = json.loads(pastebin_url)
+                    except Exception as e:
+                        print('=============Error==========', e)
                 for letter in file.document_dispatch:
                     letter.folder_id = self.deffolderid.id
                     self.deffolderid.document_dispatch = [(4, letter.id)]
